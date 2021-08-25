44ec07b5cac17fc859c45a54c50987f3a538bb62
glenn <glenn.profile@gmail.com>
2021-08-25 17:37:31 +0800

    feat: update

--- jinstall/dev-zsh.sh
+++ jinstall/dev-zsh.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-# check
+echo
+echo '# check'
+echo
 echo $SHELL
 zsh --version
 
-# install
+echo
+echo '# install'
+echo
 sudo apt install -y zsh && nl /etc/shells
 curl -L http://install.ohmyz.sh | sh
 
-# 改變你的 shell
-chsh -s /bin/zsh
+echo
+echo '# install zsh-autosuggestions extend, 輸入命令時, 會提示上一次輸入時的文字'
+echo
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# other
-# vi ~/.zshrc
+echo '----------------------------------------'
+echo '# 改變你的 shell'
+echo 'chsh -s /bin/zsh'
+echo
+echo '# config'
+echo 'vi ~/.zshrc'
+echo
+echo '    plugins=(git node npm composer github tmux zsh-autosuggestions)'
+echo
+
+
