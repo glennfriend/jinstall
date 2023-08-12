058bdf0b8d521eb53433ad3ba82d852938d06c5e
glenn <glennfriend@gmail.com>
2023-08-12 14:11:13 +0800

    feat: update

--- auto-complete-jinstall.sh
+++ auto-complete-jinstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-# TOOL_PATH=~/tools
-# SCRIPT_FILE_LIST=$(ls "${TOOL_PATH}/jinstall/jinstall/")
+TOOL_PATH=~/tools
+SCRIPT_FILE_LIST=$(ls "${TOOL_PATH}/jinstall/jinstall/")
 
-TOOL_PATH="$(dirname "$(readlink -f $0)")"
-SCRIPT_FILE_LIST=$(ls "${TOOL_PATH}/jinstall/")
+# TOOL_PATH="$(dirname "$(readlink -f $0)")"
+# SCRIPT_FILE_LIST=$(ls "${TOOL_PATH}/jinstall/")
 
 
 #echo   complete -W \"${SCRIPT_FILE_LIST}\" jinstall
