240266a25e0d25db31c06fcfaf56957117756c0a
glenn <glennfriend@gmail.com>
2023-08-12 13:19:52 +0800

    feat: update

--- jinstall/visualbox.sh
+++ jinstall/visualbox.sh
@@ -20,4 +20,10 @@ xrandr --output $(xrandr | grep " connected" | awk '{ print$1 }') --mode ${SCREE
 gsettings set org.gnome.desktop.session idle-delay 3600
 
 
+# notebook 把蓋子合起來 防止筆記本進入睡眠模式
+# Disable suspend when lid is closed
+gsettings set org.gnome.settings-daemon.plugins.power lid-close-ac-action nothing
+gsettings set org.gnome.settings-daemon.plugins.power lid-close-battery-action nothing
+
+
 
