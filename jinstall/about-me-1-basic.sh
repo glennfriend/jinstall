c7042d1968d7bdfb315425ca617df339e6fd2236
glenn guan <glenn.profile@gmail.com>
2024-07-30 16:13:19 +0800

    feat: update

--- jinstall/about-me-1-basic.sh
+++ jinstall/about-me-1-basic.sh
@@ -10,6 +10,12 @@ sudo apt install -y git git-core tig
 sudo apt install -y ack-grep
 sudo apt install -y vim vim-gui-common vim-runtime
 
+## bat = cat
+sudo apt install -y bat
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
+
+
 ## 進度顯示
 sudo apt install pv
 
